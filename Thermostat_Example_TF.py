24caac0a0079d7c48aa2781d7cd0bc760f623fd6
Jake Nelson <jake.nelson7@gmail.com>
2021-04-22 18:12:43 -0400

    Update Thermostat_Example_TF.py

--- Thermostat_Example_TF.py
+++ Thermostat_Example_TF.py
@@ -16,45 +16,47 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import pyswmm.toolkitapi as tkai
+from pyswmm import Simulation, Nodes, Links, Subcatchments, LidControls
 
 ## Compute the response for a given action and current temperature
-def respond(action, current_temp, tau):
-    return action + (current_temp - action) * math.exp(-1.0/tau)
+# def respond(action, current_temp, tau):
+#     return action + (current_temp - action) * math.exp(-1.0/tau)
 
-## Actions of a series of on, then off
-sAction = pd.Series(np.array([1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0]))
-sResponse = np.zeros(sAction.size)
+# ## Actions of a series of on, then off
+# sAction = pd.Series(np.array([1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0]))
+# sResponse = np.zeros(sAction.size)
 
-## Update the response with the response to the action
-for i in range(sAction.size):
-    ## Get last response
-    if i == 0:
-        last_response = 0
-    else:
-        last_response = sResponse[i - 1]
-    sResponse[i] = respond(sAction[i], last_response, 3.0)
+# ## Update the response with the response to the action
+# for i in range(sAction.size):
+#     ## Get last response
+#     if i == 0:
+#         last_response = 0
+#     else:
+#         last_response = sResponse[i - 1]
+#     sResponse[i] = respond(sAction[i], last_response, 3.0)
 
-## Assemble and plot
-df = pd.DataFrame(list(zip(sAction, sResponse)), columns=['action', 'response'])
-df.plot()
+# ## Assemble and plot
+# df = pd.DataFrame(list(zip(sAction, sResponse)), columns=['action', 'response'])
+# df.plot()
 
-## goal and reward
-def reward(temp):
-        delta = abs(temp - 0.5)
-        if delta < 0.1:
-            return 0.0
-        else:
-            return -delta + 0.1
+# ## goal and reward
+# def reward(temp):
+#         delta = abs(temp - 0.5)
+#         if delta < 0.1:
+#             return 0.0
+#         else:
+#             return -delta + 0.1
 
-temps = [x * 0.01 for x in range(100)]
-rewards = [reward(x) for x in temps]
+# temps = [x * 0.01 for x in range(100)]
+# rewards = [reward(x) for x in temps]
 
-fig=plt.figure(figsize=(12, 4))
+# fig=plt.figure(figsize=(12, 4))
 
-plt.scatter(temps, rewards)
-plt.xlabel('Temperature')
-plt.ylabel('Reward')
-plt.title('Reward vs. Temperature')
+# plt.scatter(temps, rewards)
+# plt.xlabel('Temperature')
+# plt.ylabel('Reward')
+# plt.title('Reward vs. Temperature')
 
 ###-----------------------------------------------------------------------------
 ## Imports
@@ -63,9 +65,10 @@ from tensorforce.agents import Agent
 
 
 
+
 ###-----------------------------------------------------------------------------
 ### Environment definition
-class ThermostatEnvironment(Environment):
+class SWMM_env(Environment):
     """This class defines a simple thermostat environment.  It is a room with
     a heater, and when the heater is on, the room temperature will approach
     the max heater temperature (usually 1.0), and when off, the room will
@@ -74,17 +77,44 @@ class ThermostatEnvironment(Environment):
     """
    # J: current_temp could be changed to current_flow which is Links(sim)["8"].flow*35.3147
    # J: this is where we define everything and start the swmm model
-    def __init__(self):
+    def __init__(self, inp_file): # the max depth of the ponds is 2 meters. This I believe is also an intial setting
         ## Some initializations.  Will eventually parameterize this in the constructor.
-        self.tau = 3.0
-        self.current_temp = np.random.random(size=(1,))
-
+        self.modelfile = inp_file
+        self.sim = Simulation(self.modelfile)
+        
+        self.controlstep = 900 #in seconds
+        self.sim.step_advance(self.controlstep)
+        node_object = Nodes(self.sim)
+        self.P1 = node_object["P1"]
+        self.P2 = node_object["P2"]
+        #self.depth = depth
+        # self.P1.depth = self.depth
+        # self.P2.depth = self.depth
+        
+        # init link objects - includes downstream link and both orifices
+        link_object = Links(self.sim)
+        self.L8 = link_object["8"]
+        #self.flow = flow
+        #self.L8.flow = self.L8.flow
+        self.O1 = link_object["1"]
+        self.O2 = link_object["2"]
+        
+        
+        self.sim.start()
+        if self.sim.current_time == self.sim.start_time:
+            #initially sets the orifices at half way open
+            self.O1.target_setting = 0.5
+            self.O2.target_setting = 0.5
+        sim_len = self.sim.end_time - self.sim.start_time
+        self.T = int(sim_len.total_seconds()/self.controlstep)
+        self.state = np.concatenate([np.asarray([self.P1.depth, self.P2.depth, self.L8.flow, 
+                        self.P1.flooding, self.P2.flooding, self.O1.current_setting, self.O2.current_setting])])
         super().__init__()
 
 
-# J: would the states be the depth in P1 and P2?
-    def states(self):
-        return dict(type='float', shape=(1,), min_value=0.0, max_value=1.0)
+# J: I think this should consider the 6 states referenced in the self.state line above. 
+    # def states(self):
+    #     return dict(type='float', shape=(6,))
 
 # J: would the actions be the position of the valves at P1 and P2? Would we need to figure
 # J: out how to program the lid for this section, given two valve positions?
@@ -99,31 +129,51 @@ class ThermostatEnvironment(Environment):
     #(original comment) Optional, should only be defined if environment has a natural maximum
     #(original comment) episode length
     # I can change the frequency (maybe 15 minutes, maybe something else)
-    def max_episode_timesteps(self):
-        return super().max_episode_timesteps()
+    # def max_episode_timesteps(self):
+    #     return super().max_episode_timesteps()
 
 
 ### J: Not sure how this works with the swmm model.
     # Optional
     def close(self):
-        super().close()
+        self.sim.report()
+        self.sim.close()
 
 ### J: Not sure how this works with the swmm model.
     def reset(self):
         """Reset state.
         """
         # state = np.random.random(size=(1,))
-        self.timestep = 0
-        self.current_temp = np.random.random(size=(1,))
-        return self.current_temp
+        
+        self.sim.close()
+        self.sim = Simulation(self.modelfile)
+        self.sim.step_advance(self.controlstep)
+        node_object = Nodes(self.sim)
+        self.P1 = node_object["P1"]
+        self.P2 = node_object["P2"]
+        self.P1.depth = self.depth
+        self.P2.depth = self.depth
+        
+        link_object = Links(self.sim)
+        self.L8 = link_object["8"]
+        self.L8.flow = self.flow
+        self.sim.start()
+        
+        if self.sim.current_time == self.sim.start_time:
+            self.L8.target_setting = 0
+            
+        self.state = np.concatenate([np.asarray([self.P1.depth, self.P2.depth, self.L8.flow, 
+                        self.P1.flooding, self.P2.flooding, self.O1.current_setting, self.O2.current_setting])])
+        
+        return self.state
 
 # J: The response should be the total flow in Pipe8, correct? We might not need this piece
-    def response(self, action):
-        """Respond to an action.  When the action is 1, the temperature
-        exponentially decays approaches 1.0.  When the action is 0,
-        the current temperature decays towards 0.0.
-        """
-        return action + (self.current_temp - action) * math.exp(-1.0 / self.tau)
+    # def response(self, action):
+    #     """Respond to an action.  When the action is 1, the temperature
+    #     exponentially decays approaches 1.0.  When the action is 0,
+    #     the current temperature decays towards 0.0.
+    #     """
+    #     return action + (self.current_temp - action) * math.exp(-1.0 / self.tau)
 
 # J: The reward is 0 if the flow in Pipe8 is betwee, X1 and X2, or else we could have
 # J: it negatively increase as the flow increases/decreases beyond the boundary. 
@@ -133,7 +183,10 @@ class ThermostatEnvironment(Environment):
         
         Return the value within the numpy array, not the numpy array.
         """
-        delta = abs(self.current_temp - 0.5)
+        
+        # the reward should take into consideration flow being outside of desired range; should it also consider flooding?
+        # reward needs to be based on the state - need to pass in new state information to calculate reward
+        delta = abs(self.L8.flow- 0.25)
         if delta < 0.1:
             return 0.0
         else:
@@ -142,13 +195,19 @@ class ThermostatEnvironment(Environment):
 # check this with the step function in Ben's code
     def execute(self, actions):
         ## Check the action is either 0 or 1 -- heater on or off.
-        assert actions == 0 or actions == 1
-
-        ## Increment timestamp
-        self.timestep += 1
         
-        ## Update the current_temp
-        self.current_temp = self.response(actions)
+        self.O1.target_setting = actions[0]
+        self.O2.target_setting = actions[1]
+        
+        self.sim.__next__()
+        self.state = np.concatenate([np.asarray([self.P1.depth,self.P2.depth,self.P1.flooding,
+                                                 self.P2.flooding,self.L8.current_setting])])
+
+        # ## Increment timestamp
+        # self.timestep += 1
+        
+        # ## Update the current_temp
+        # self.current_temp = self.response(actions)
         
         ## Compute the reward
         reward = self.reward_compute()
@@ -158,16 +217,17 @@ class ThermostatEnvironment(Environment):
         ## terminal == True means it is done.
         terminal = False
         
-        return self.current_temp, terminal, reward
+        return self.state, terminal, reward 
     
 
 ###-----------------------------------------------------------------------------
 ### Create the environment
 ###   - Tell it the environment class
 ###   - Set the max timestamps that can happen per episode
+# removed timestamps as the model should take care of that.
+
 environment = environment = Environment.create(
-    environment=ThermostatEnvironment,
-    max_episode_timesteps=100)
+    environment=SWMM_env('theta.inp'))
 
 # agent setup
 agent = Agent.create(
@@ -187,18 +247,18 @@ environment.reset()
 ## class, like to set the initial temp to a custom value, like here,
 ## you need to access the `environment` member of this wrapped class.
 ## That is why you see the way to set the current_temp like below.
-environment.current_temp = np.array([0.5])
-states = environment.current_temp
+# environment.current_temp = np.array([0.5])
+# states = environment.current_temp
 
 internals = agent.initial_internals()
 terminal = False
 
 ### Run an episode
-temp = [environment.current_temp[0]]
+temp = [environment.state[2]]
 while not terminal:
-    actions, internals = agent.act(states=states, internals=internals, independent=True)
+    actions, internals = agent.act(states= environment.state, internals=internals, independent=True)
     states, terminal, reward = environment.execute(actions=actions)
-    temp += [states[0]]
+    #temp += [states[0]]
     
 ### Plot the run
 plt.figure(figsize=(12, 4))
@@ -236,17 +296,17 @@ environment.reset()
 ## you need to access the `environment` member of this wrapped class.
 ## That is why you see the way to set the current_temp like below.
 environment.current_temp = np.array([1.0])
-states = environment.current_temp
+states = environment.state
 
 internals = agent.initial_internals()
 terminal = False
 
 ### Run an episode
-temp = [environment.current_temp[0]]
+#temp = [environment.current_temp[0]]
 while not terminal:
     actions, internals = agent.act(states=states, internals=internals, independent=True)
     states, terminal, reward = environment.execute(actions=actions)
-    temp += [states[0]]
+    #temp += [states[0]]
 
 ### Plot the run
 plt.figure(figsize=(12, 4))
